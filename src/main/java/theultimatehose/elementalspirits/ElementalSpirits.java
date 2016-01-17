5ddc032a8852bd2bfc4b4859ea93fda67973a949
TheUltimateHose <TheUltimateHose@gmail.com>
2016-01-17 21:35:55 +0100

    Initial commit

---
+++ src/main/java/theultimatehose/elementalspirits/ElementalSpirits.java
@@ -0,0 +1,27 @@
+package theultimatehose.elementalspirits;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import theultimatehose.util.Util;
+
+@Mod(modid = Util.MOD_ID, version = Util.VERSION)
+public class ElementalSpirits {
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/theultimatehose/util/Util.java
@@ -0,0 +1,11 @@
+package theultimatehose.util;
+
+import java.util.Locale;
+
+public class Util {
+
+    public static final String MOD_ID = "ElementalSpirits";
+    public static final String MOD_ID_LOWER = MOD_ID.toLowerCase(Locale.ROOT);
+    public static final String VERSION = "r0";
+
+}
